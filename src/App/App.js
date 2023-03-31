3a01aeefeb7243cd49e0705ab1e4c6659dd0bf46
Athos Matos <ladiesman217.as@gmail.com>
2023-03-31 02:09:33 -0300

    up

--- src/App/App.js
+++ src/App/App.js
@@ -102,7 +102,7 @@ const App = () =>
         let divided = []
         for (let i = 0; i < dataArray.length; i++)
         {
-            divided.push(((dataArray[i]/128) - 1)* 100) 
+            divided.push((dataArray[i]/128) - 1)
         }
         
 
